4a4354c71609ae1f24a5ec59475bf0c979d32531
Carlos Astori <tadeu.astori@gmail.com>
2022-05-23 20:20:14 -0400

    publish

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/party-apportionment'),
+      dir: require('path').join(__dirname, './coverage/RateioJambalaya'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'party-apportionment'`, () => {
+  it(`should have as title 'RateioJambalaya'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('party-apportionment');
+    expect(app.title).toEqual('RateioJambalaya');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('party-apportionment app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('RateioJambalaya app is running!');
   });
 });
